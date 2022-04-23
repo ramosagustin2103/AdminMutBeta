5d4db91c0121690f65ea0840685fef0cea107a6f
Tomas Ramos <rtomasfr1020@gmail.com>
2022-04-22 21:13:03 -0300

    primer commit

--- expensas_pagas/manager.py
+++ expensas_pagas/manager.py
@@ -605,7 +605,7 @@ class NewExp():
 			#datetime(2022, 4, 15),
 			#datetime(2022, 4, 16),
 			#datetime(2022, 4, 17),
-			#datetime(2022, 4, 18),
+			datetime(2022, 4, 18),
 			datetime(2022, 4, 19),
 			datetime(2022, 4, 20),
 			datetime(2022, 4, 21),			
